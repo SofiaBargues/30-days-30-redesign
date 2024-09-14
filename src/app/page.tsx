adcccdb81d019512fbc14465ea786def3c8a60c0
Sofia Bargues <barguessofia@gmail.com>
2024-09-14 10:05:49 +0100

    its working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,5 @@
+import { reverse } from "dns";
 import Image from "next/image";
-
-const DAYS = [
-  "1",
-  "2, 3 and 4",
-  "5, 6 and 7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-  "23",
-  "24",
-  "25",
-  "26",
-  "27",
-  "28",
-  "29",
-  "30",
-];
 
 const YOUTUBE_PLAYLIST_ITEMS_API =
   "https://www.googleapis.com/youtube/v3/playlistItems";
@@ -38,14 +10,15 @@ export default async function Home() {
     `${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&playlistId=${PLAYLIST_ID}&maxResults=30&key=${process.env.YOUTUBE_API_KEY}`
   );
   const data = await res.json();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed lg:text-2xl left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          30 Days 30 Projects
+          30 Days of Redesigning
         </p>
         <div className="fixed lg:text-xl bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          Project based learning challenge
+          Learning journey
         </div>
       </div>
 
@@ -60,38 +33,40 @@ export default async function Home() {
       ></div>
 
       <ul className="m-10 gap-6 mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
-        {data.items.map((item: any, index: number) => {
-          const { id, snippet = {} } = item;
-          const { title, thumbnails = {}, resourceId } = snippet;
-          const { medium = {} } = thumbnails;
-          return (
-            <li
-              key={id}
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            >
-              <a
-                className="flex flex-col items-center gap-4"
-                href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}
+        {data.items
+          .map((item: any, index: number) => {
+            const { id, snippet = {} } = item;
+            const { title, thumbnails = {}, resourceId } = snippet;
+            const { medium = {} } = thumbnails;
+            return (
+              <li
+                key={id}
+                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
               >
-                <h2 className={` text-2xl font-semibold`}>
-                  {item.snippet.title}
-                </h2>
-                <Image
-                  className="rounded-lg"
-                  width={medium.width}
-                  height={medium.height}
-                  src={medium.url}
-                  alt={title}
-                ></Image>
-                <p
-                  className={`w-full text-center text-sm opacity-50 text-balance`}
+                <a
+                  className="flex flex-col items-center gap-4"
+                  href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}
                 >
-                  {title}
-                </p>
-              </a>
-            </li>
-          );
-        })}
+                  <h2 className={` text-2xl font-semibold`}>
+                    {item.snippet.title}
+                  </h2>
+                  <Image
+                    className="rounded-lg"
+                    width={medium.width}
+                    height={medium.height}
+                    src={medium.url}
+                    alt={title}
+                  ></Image>
+                  <p
+                    className={`w-full text-center text-sm opacity-50 text-balance`}
+                  >
+                    {title}
+                  </p>
+                </a>
+              </li>
+            );
+          })
+          .reverse()}
       </ul>
     </main>
   );
